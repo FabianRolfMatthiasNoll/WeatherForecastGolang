67ee41196398158dce3dc45b301702ceb2d3aff2
fanoll <Fabian.Noll@karlstorz.com>
2023-02-14 08:40:45 +0100

    - added call to the api interface

--- main.go
+++ main.go
@@ -10,7 +10,8 @@ func main() {
 	if argLength > 0 {
 		arg := os.Args[1]
 		Longitude, Latitude := getCoordinates(arg)
-		fmt.Println(Longitude, Latitude)
+		fmt.Println(Longitude, Latitude) //Please remove later on
+		getWeatherData(Longitude, Latitude)
 	} else {
 		fmt.Println("Hello World")
 	}
