4247e8b9feac8de005f365c3faf06f803cdb8d0c
fanoll <Fabian.Noll@karlstorz.com>
2023-02-14 08:42:09 +0100

    - added an interface that requests weather data from an open API based on the provided longitude and latitude

---
+++ api_interface.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func getWeatherData(long, lat float64) {
+	url := fmt.Sprintf("https://api.open-meteo.com/v1/forecast?latitude=%f&longitude=%f&hourly=temperature_2m,relativehumidity_2m,rain,showers,cloudcover", lat, long)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Println("couldn't request data")
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}(res.Body)
+
+	body, readErr := io.ReadAll(res.Body)
+	if readErr != nil {
+		fmt.Println(readErr.Error())
+	}
+	fmt.Println(string(body))
+}
