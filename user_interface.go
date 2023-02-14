9717586566541ad49b06b295ab11e1e4868293b2
fanoll <Fabian.Noll@karlstorz.com>
2023-02-14 13:40:25 +0100

    - added function to neatly display the different weather stats

--- user_interface.go
+++ user_interface.go
@@ -1,5 +1,50 @@
 package main
 
-func displayWeather(firstIndex, LastIndex int) {
+import (
+	"fmt"
+	"github.com/jedib0t/go-pretty/v6/table"
+	"os"
+)
 
+func displayWeather(firstIndex, LastIndex int, weather Weather, cityName string) {
+
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"Time", "Temperature", "Rain", "Showers", "Cloud%", "Humidity"})
+
+	fmt.Println("You are currently viewing the weather of: ", cityName)
+
+	for i := firstIndex; i <= LastIndex; i = i + 2 {
+		time := weather.Hourly.Time[i][11:13] + ":00"
+		temp := weather.Hourly.Temperature2M[i]
+		rain := weather.Hourly.Rain[i]
+		shower := weather.Hourly.Showers[i]
+		clouds := weather.Hourly.Cloudcover[i]
+		humidity := weather.Hourly.Relativehumidity2M[i]
+		//fmt.Print("|", weather.Hourly.Time[i][11:13], ":00")
+		t.AppendRow([]interface{}{time, temp, rain, shower, clouds, humidity})
+	}
+
+	//fmt.Print("\nTemperature: ")
+	for i := firstIndex; i <= LastIndex; i = i + 2 {
+		//fmt.Print("|", weather.Hourly.Temperature2M[i])
+	}
+	//fmt.Print("\nCloudcoverage: ")
+	for i := firstIndex; i <= LastIndex; i = i + 2 {
+		//fmt.Print("|", weather.Hourly.Cloudcover[i])
+	}
+	//fmt.Print("\nHumidity: ")
+	for i := firstIndex; i <= LastIndex; i = i + 2 {
+		//fmt.Print("|", weather.Hourly.Relativehumidity2M[i])
+	}
+	//fmt.Print("\nRain: ")
+	for i := firstIndex; i <= LastIndex; i = i + 2 {
+		//fmt.Print("|", weather.Hourly.Rain[i])
+	}
+	//fmt.Print("\nShowers: ")
+	for i := firstIndex; i <= LastIndex; i = i + 2 {
+		//fmt.Print("|", weather.Hourly.Showers[i])
+	}
+	t.SetStyle(table.StyleColoredBlackOnBlueWhite)
+	t.Render()
 }
