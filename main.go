59ca6c0f3a9e5a05794dc8e2832e43e9d3d99b03
fanoll <Fabian.Noll@karlstorz.com>
2023-02-14 07:40:24 +0100

    - added a call to the cityParser function

--- main.go
+++ main.go
@@ -9,7 +9,8 @@ func main() {
 	argLength := len(os.Args[1:])
 	if argLength > 0 {
 		arg := os.Args[1]
-		fmt.Println(arg)
+		Longitude, Latitude := getCoordinates(arg)
+		fmt.Println(Longitude, Latitude)
 	} else {
 		fmt.Println("Hello World")
 	}
