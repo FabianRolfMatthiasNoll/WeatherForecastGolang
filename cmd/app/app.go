15c5061fd8dfead0ccf4abc5af268874bf6aa1b5
fanoll <Fabian.Noll@karlstorz.com>
2023-02-17 08:13:49 +0100

    - exported the logic to the manager.go and added Dependency Injection

--- cmd/app/app.go
+++ cmd/app/app.go
@@ -2,8 +2,6 @@ package app
 
 import (
 	"WeatherForecastGolang/internal"
-	"fmt"
-	"strings"
 	"time"
 )
 
@@ -17,45 +15,11 @@ type CSVParser interface {
 }
 
 type DataParser interface {
-	ParseWeather(data []byte) (internal.Weather, int, int, error)
+	ParseWeather([]byte, time.Time) (*internal.Weather, int, int, error)
 }
 
-// Build: go build -o ..\bin\Weather_Forecast_App
 func Run(cityName string) {
-	var Longitude, Latitude float64
-	var err error
 
-	var weatherAPI internal.API
-	var api APIHandler = weatherAPI
-
-	var cityParser internal.CityData
-	var city CSVParser = cityParser
-
-	var weatherParser internal.Weather
-	var weatherParsed DataParser = weatherParser
-
-	for Longitude == 0 && Latitude == 0 {
-		if len(cityName) > 0 {
-			Longitude, Latitude, err = city.GetCoordinates(cityName)
-		} else {
-			cityName = internal.GetCity()
-			Longitude, Latitude, err = city.GetCoordinates(cityName)
-		}
-	}
-	if err != nil {
-		fmt.Println(err.Error())
-		time.Sleep(30 * time.Second)
-	}
-	apiUrl := api.CreateAPILink(Longitude, Latitude)
-	weatherData, err := api.GetData(apiUrl)
-	if err != nil {
-		fmt.Println(err.Error())
-		time.Sleep(30 * time.Second)
-	}
-	weather, firstEntry, lastEntry, err := weatherParsed.ParseWeather(weatherData)
-	if err != nil {
-		fmt.Println(err.Error())
-		time.Sleep(30 * time.Second)
-	}
-	internal.DisplayWeather(firstEntry, lastEntry, weather, strings.ToUpper(cityName))
+	manager := NewManager(internal.API{}, internal.CityData{}, internal.Weather{}, time.Now())
+	manager.Execute(cityName)
 }
